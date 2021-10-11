335f57e74836b86da3cdcbc202e9417a6feb2efe
Katherina <fedoseeva2292@gmail.com>
2021-10-11 11:01:13 +0200

    linter

--- src/runApp.jsx
+++ src/runApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './Сomponents/App.jsx';
 
 const render = () => {
   const vdom = document.getElementById('chat');
