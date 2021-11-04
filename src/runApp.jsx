ee6d721f28a841cf8421af613756421396f7fe8f
Katherina <fedoseeva2292@gmail.com>
2021-11-04 18:07:29 +0100

    changed entry point

--- src/runApp.jsx
+++ src/runApp.jsx
@@ -8,6 +8,7 @@ import { rollbarConfig, rollbarInstance } from './rollbar.js';
 import App from './Сomponents/App.jsx';
 import store from './store/index.js';
 import resources from './locales/index.js';
+import { SocketProvider } from './contexts/index.js';
 
 const render = async () => {
   await i18n.use(initReactI18next).init({
@@ -24,7 +25,9 @@ const render = async () => {
     <Provider store={store}>
       <RollBarProvider config={rollbarConfig} instance={rollbarInstance}>
         <I18nextProvider i18n={i18n}>
-          <App />
+          <SocketProvider>
+            <App />
+          </SocketProvider>
         </I18nextProvider>
       </RollBarProvider>
     </Provider>
